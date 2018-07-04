21b65ff1bd8d3c71df30bad637f7221ec05471c3
CHIK SUM <chiksum.wong@gmail.com>
2018-07-04 23:55:01 +0800

    update

--- src/components/math_formula/Chinese.vue
+++ src/components/math_formula/Chinese.vue
@@ -1,15 +1,15 @@
 <template>
   <Page class="page">
     <ActionBar class="action-bar" title="中文版">
-      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$router.push('/home')"/>
+      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$router.push('/home')" />
     </ActionBar>
 
     <Scrollview>
       <StackLayout class="home-panel">
-        <listView class="list-group" for="chapter in chapters" @itemTap="onItemTap" style="height:3500px"> 
+        <listView class="list-group" for="chapter in chapters" @itemTap="onItemTap" style="height:3500px">
           <v-template>
             <FlexboxLayout flexDirection="row" class="list-group-item">
-              <Label :text="chapter.title" class="list-group-item-heading" style="width: 60%"/>
+              <Label :text="chapter.title" class="list-group-item-heading" style="width: 60%" />
             </FlexboxLayout>
           </v-template>
         </listView>
@@ -20,106 +20,115 @@
 </template>
 
 <script>
-import Formula from'./Formula';
+  import Formula from './Formula';
 
   export default {
-    data(){
+    data() {
       return {
-        chapters:[
-          {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+        chapters: [{
+            imagePath: [
+              "~/images/hkdse_math_formula/chinese_version/2/0.png"
+            ],
             title: "單元二",
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter3.png",
+            imagePath: [
+              "~/images/hkdse_math_formula/chinese_version/3/0.png",
+              "~/images/hkdse_math_formula/chinese_version/3/1.png",
+              "~/images/hkdse_math_formula/chinese_version/3/2.png",
+              "~/images/hkdse_math_formula/chinese_version/3/3.png",
+              "~/images/hkdse_math_formula/chinese_version/3/4.png",
+            ],
             title: "單元三"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter4.png",
+            imagePath: [
+              "~/images/hkdse_math_formula/chinese_version/2/0.png"
+            ],
             title: "單元四"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元五"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元六"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元七"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元八"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元九"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十一"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十二"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十三"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十四"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十五"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十六"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十七"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十八"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元十九"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元二十"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元二十一"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元二十二"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元二十三"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元二十四"
           },
           {
-            imagePath: "~/images/hkdse_math_formula/chinese_version/chapter2.png",
+            imagePath: ["~/images/hkdse_math_formula/chinese_version/2/0.png"],
             title: "單元二十五"
           }
         ]
@@ -127,12 +136,12 @@ import Formula from'./Formula';
     },
 
     methods: {
-      onItemTap: function(args){
+      onItemTap: function (args) {
         console.log('Item with index : ' + args.item.title + ' tapped');
 
         this.$navigateTo(Formula, {
-          context:{
-            propsData:{
+          context: {
+            propsData: {
               item: args.item
             }
           }
--- src/components/math_formula/Formula.vue
+++ src/components/math_formula/Formula.vue
@@ -1,12 +1,19 @@
 <template>
   <Page class="page">
     <ActionBar class="action-bar" :title="this.item.title">
-      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="onBackTap"/>
+      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="onBackTap" />
     </ActionBar>
 
-      <StackLayout>
-        <Image :src="this.item.imagePath" />
-      </StackLayout>
+    <StackLayout>
+      <!-- <Image for="image in images" :src="image" stretch="aspectFit" /> -->
+
+      <listView class="list-group" for="image in images">
+        <v-template>
+          <Image :src="image" stretch="aspectFit" />
+        </v-template>
+      </listView>
+
+    </StackLayout>
 
   </Page>
 </template>
@@ -14,20 +21,28 @@
 <script>
   export default {
     props: [
-        'item'
+      'item'
     ],
 
-    data () {
+    data() {
       return {
+        images: []
       }
     },
 
     methods: {
-        onBackTap(){
-            console.log("Back");
-            this.$navigateBack();
-        }
+      onBackTap() {
+        console.log("Back");
+        this.$navigateBack();
+      }
     },
+
+    mounted() {
+      console.log(this.item.imagePath);
+      this.images = this.item.imagePath;
+      console.log(this.images);
+      console.log(this.images[0]);
+    }
 
   };
 </script>
